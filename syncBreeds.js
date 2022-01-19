686ff7a786b11aac2db0b45e672f6d0c27687e37
Logan Woolf <loganwoolf@gmail.com>
2022-01-19 03:42:41 +0000

    Create M&C tests for sync breed details

--- syncBreeds.js
+++ syncBreeds.js
@@ -6,5 +6,7 @@ const breedDetails = function(breed) {
 };
 
 // get the return value right away from the function
-const bombay = breedDetails('Bombay');
-console.log(bombay.slice(0,35) + '...'); //=> prints out the description for that breed
+const bombay = breedDetails('Tiger');
+// console.log(bombay.slice(0,35) + '...'); //=> prints out the description for that breed
+
+module.exports = breedDetails;
