6bac7a0743632fa99ca2b61bae1b06b574305da4
Logan Woolf <loganwoolf@gmail.com>
2022-02-04 15:06:22 +0000

    Modify and test asyncBreeds

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,31 +1,25 @@
-const fs = require('fs');
+const { readFile } = require('fs');
 
-const breedDetailsGetter = function(breed, outputCallback) {
-  // console.log('breedDetailsFromFile: Calling readfile...');
-
-  fs.readFile(`./data/${breed}.txt`
+const breedDetails = function(breed, callback) {
+  readFile(`./data/${breed}.txt`
     , 'utf8'
     , (error, data) => {
-
-      // console.log(`In readfile's Callback: ${data.slice(0,20)}...`);
-
-      !error ? outputCallback(data) : errorHandler(error);
-
+      return !error ? callback(data) : callback(undefined);
     });
-
-  // Issue: attempting to return things from here will not work either
-
 };
 
-const breedDetails = (data) => {
-  console.log(`breedDetails: ${data.slice(0,40)}...`);
-  return data;
-};
-const errorHandler = (error) => {
-  console.log(`Error! Could not find file: ${error.path}`);
-  // console.log(error);
-};
+// const logDetails = (data) => {
+//   // console.log(`breedDetails: ${data}`);
+//   return data;
+// };
+// const logError = (error) => {
+//   // console.log(error);
+//   return undefined;
+// };
 
-breedDetailsGetter('Bombay', breedDetails);
-breedDetailsGetter('Tiger', breedDetailsGetter);
-// console.log('bombay return value: ', bombay);
+// console.log(
+//   // breedDetails('Tiger')
+//   breedDetails('Bombay')
+// );
+
+module.exports = { breedDetails };
